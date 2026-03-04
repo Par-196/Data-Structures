ab3f8468a0ff3ef686a5cb135fe0f0c9dddc8cc1
Par-196 <yuraparkasevych@gmail.com>
2026-03-04 19:50:38 +0100

    Add DoublyLinkedList project and node logic
    Add a new DoublyLinkedListProject to the solution and wire it into DataStructures (solution .sln updated, and DataStructures.csproj gets a ProjectReference). Include Models (Data.cs, DoublyLinkedList.cs, Noda.cs) in the project file. Implement a basic generic DoublyLinkedList<T> skeleton (Head, Tail, Count, constructor, AddAfter) and add linking helper methods to Noda (ConnectNewNodaToNext, ConnectNewNodaToPrevious) plus minor fixes to node field initialization. Also includes Visual Studio workspace/DocumentLayout updates (.vs files) to reflect opened documents.

--- DataStructures/DoublyLinkedList/Models/DoublyLinkedList.cs
+++ DataStructures/DoublyLinkedList/Models/DoublyLinkedList.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList.Models
 {
-    public class DoublyLinkedList
+    public class DoublyLinkedList<T>
     {
+        private Noda Head { get; set; }
+        private Noda Tail { get; set; }
+        private int Count { get; set; }
+
+
+        public DoublyLinkedList()
+        { 
+            
+        }
+
+        public void AddAfter(Noda node, Noda newNode)
+        {
+            node.ConnectNewNodaToNext(newNode);
+            newNode.ConnectNewNodaToPrevious(node);
+        }
+
+        
 
     }
 }
--- DataStructures/DoublyLinkedList/Models/Noda.cs
+++ DataStructures/DoublyLinkedList/Models/Noda.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DoublyLinkedList.Models
 {
     public class Noda
     {
         private Data Date { get; set; }
-        private Noda PreviousNode { get; set; }
         private Noda NextNode { get; set; }
+        private Noda PreviousNode { get; set; }
+        
 
         public Noda(Data data) 
         {
             Date = data;
-            PreviousNode = null;
             NextNode = null;
+            PreviousNode = null;
         }
+
+        public void ConnectNewNodaToNext(Noda newNode)
+        {
+            newNode.NextNode = NextNode;
+
+            NextNode = newNode;
+
+            NextNode.PreviousNode = newNode;
+        }
+
+        public void ConnectNewNodaToPrevious(Noda node)
+        {
+            PreviousNode = node;
+        }
+
     }
 }
