1e6659d53c867884cdb738ac6e94d2e6e5700d6c
Par-196 <yuraparkasevych@gmail.com>
2026-04-01 18:50:51 +0200

    Add BinaryTree project to solution
    Add a new BinaryTree project (BinaryTreeProject) to the solution: adds BinaryTreeProject.csproj, App.config, Program.cs, Properties/AssemblyInfo.cs, and model classes (BinaryTree.cs, Root.cs). Update DataStructures.sln and DataStructures.csproj to include and reference the new project. Also include IDE workspace/layout updates (.vs DocumentLayout and .suo) and regenerated build cache (obj/*.AssemblyReference.cache). The BinaryTree project contains a minimal Program and basic Root/BinaryTree models to start implementation.

---
+++ DataStructures/BinaryTreeProject/Models/Root.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeProject.Models
+{
+    public class Root
+    {
+        public int value { get; set; }
+        public Root Left { get; set; }
+        public Root Right { get; set; }
+    }
+}
