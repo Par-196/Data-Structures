f1be4586ec7c4d55880303ef7b1f2f9952e8004c
Par-196 <yuraparkasevych@gmail.com>
2026-04-06 00:33:48 +0200

    Add CustomDictionary and demo usage
    Introduce a generic CustomDictionary<TKey,TValue> implementation with Add/Remove/TryGetValue/TryAdd/Contains/Clear/Print and IEnumerable support. Update DictionaryProject.csproj to include the new Models/CustomDictionary.cs and modify Program.cs to instantiate the dictionary and print sample entries. Also include IDE document layout (.vs) updates reflecting the new files opened in the workspace.

---
+++ DataStructures/DictionaryProject/Models/CustomDictionary.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject.Models
+{
+    public class CustomDictionary<TKey, TValue> : IEnumerable
+    {
+        private List<KeyValuePair<TKey, TValue>> _list { get; set; }
+
+        private int _Count;
+
+        public CustomDictionary()
+        {
+            _list = new List<KeyValuePair<TKey, TValue>>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(item.Key, key))
+                {
+                    Console.WriteLine("Key already exists");
+                    return;
+                }
+            }
+            _list.Add(new KeyValuePair<TKey, TValue>(key, value));
+            _Count++;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(key, item.Key))
+                { 
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TValue>.Default.Equals(value, item.Value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Remove(TKey key)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(key, item.Key))
+                { 
+                    _list.Remove(item);
+                    _Count--;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Remove(TKey key, out TValue value)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(key, item.Key))
+                {
+                    _list.Remove(item);
+                    _Count--;
+                    value = item.Value;
+                    return true;
+                }
+            }
+            value = default(TValue);
+            return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(key, item.Key))
+                {
+                    value = item.Value;
+                    return true;
+                }
+            }
+            value = default(TValue);
+            return false;
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            foreach (var item in _list)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(item.Key, key))
+                {
+                    Console.WriteLine("Key already exists");
+                    return false;
+                }
+            }
+            _list.Add(new KeyValuePair<TKey, TValue>(key, value));
+            _Count++; 
+            return true;
+        }
+
+        public void PrintDictionary()
+        {
+            foreach (var item in _list)
+            {
+                Console.WriteLine($"Key - {item.Key}, Value - {item.Value}");
+            }
+        }
+
+        public int Count()
+        {
+            return _Count;
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach (var item in _list)
+            {
+                yield return item;
+                //_list.GetEnumerator();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- DataStructures/DictionaryProject/Program.cs
+++ DataStructures/DictionaryProject/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using DictionaryProject.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +12,22 @@ namespace DictionaryProject
 {
     public class Program
     {
-
-        // CustomDictionary<TKey, TValue)
-        // _list | List<KeyValuePair<TKey, TValue>>
-
         static void Main(string[] args)
         {
-            //Artur | 5
-            // iterate all _list | add Artur | if key exists drop an error
+            CustomDictionary<int, string> customDictionary = new CustomDictionary<int, string>();
+
+            customDictionary.Add(1, "Alice");
+            customDictionary.Add(2, "Bob");
+            customDictionary.Add(3, "Charlie");
+            customDictionary.Add(4, "Yura");
+            customDictionary.Add(5, "David");
+            customDictionary.Add(6, "Eva");
+            customDictionary.Add(7, "Frank");
+            customDictionary.Add(8, "Grace");
+            customDictionary.Add(9, "Hannah");
+            customDictionary.Add(10, "Ivan");
+
+            customDictionary.PrintDictionary();
         }
     }
 }
