e5f19f3d4f4ce970e7fcc00eddc77f5dbd826c82
Par-196 <yuraparkasevych@gmail.com>
2026-04-05 03:18:02 +0200

    Add DictionaryProject and update solution/layout
    Add a new DictionaryProject to the solution (App.config, DictionaryProject.csproj, Program.cs, Properties/AssemblyInfo.cs) with GUID {37D6C354-1E9D-4DBB-813A-B0CE28B08127}. Update DataStructures.sln to include the new project and its build configurations. Update DataStructures/Program.cs to add a using for DoublyLinkedListProject.Models. Update Visual Studio workspace files (.vs DocumentLayout.json and backup) to reflect added documents and adjust document indices/selection; some IDE/cache binary files under obj and .vs were also updated.

--- DataStructures/DictionaryProject/Program.cs
+++ DataStructures/DictionaryProject/Program.cs
@@ -1,16 +1,16 @@
-﻿using DoublyLinkedListProject.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataStructures
+namespace DictionaryProject
 {
     public class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
